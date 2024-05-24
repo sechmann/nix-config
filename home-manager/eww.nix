5aba626463d85494a480f97fceda7cf3e87a636b
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2024-05-24 09:21:13 +0200

    add eww deps

--- home-manager/eww.nix
+++ home-manager/eww.nix
@@ -1,5 +1,16 @@
-{ config, ... }:
+{ pkgs, ... }:
 {
+
+  # dependencies for eww
+  home.packages = with pkgs; [
+    bash
+    gawk
+    coreutils # for `stdbuf` and `seq`
+    gnugrep
+    socat
+    jq
+    python3
+  ];
   programs.eww = {
     enable = true;
     configDir = ./eww;
