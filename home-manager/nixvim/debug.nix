9ffeb6c7c7096a56d04b5b108b2262ff15defab9
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2024-06-12 09:19:31 +0200

    nvim: add debugger

--- home-manager/nixvim/debug.nix
+++ home-manager/nixvim/debug.nix
@@ -1,5 +1,13 @@
 {...}: {
   plugins.dap = {
     enable = true;
+    extensions = {
+      dap-go = {
+        enable = true;
+      };
+      dap-ui = {
+        enable = true;
+      };
+    };
   };
 }
