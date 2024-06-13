24fd6252c2f3402a07ea737a822a6cf780de8dcb
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2024-06-13 11:46:35 +0200

    export more sway vars

--- system/greetd.nix
+++ system/greetd.nix
@@ -4,6 +4,9 @@
   ...
 }: let
   swayRun = pkgs.writeShellScript "sway-run" ''
+    export SDL_VIDEODRIVER=wayland
+    export _JAVA_AWT_WM_NONREPARENTING=1
+    export QT_QPA_PLATFORM=wayland
     export XDG_SESSION_TYPE=wayland
     export XDG_SESSION_DESKTOP=sway
     export XDG_CURRENT_DESKTOP=sway
