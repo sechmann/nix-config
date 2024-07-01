96caa62787f51b7d14955470ab18354fb53350ea
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2024-07-01 09:47:08 +0200

    update naisdevice

--- flake.nix
+++ flake.nix
@@ -60,7 +60,7 @@
             services.naisdevice.enable = true;
             environment.systemPackages = [naisdevice.packages.${system}.naisdevice];
           }
-          naisdevice.nixosModules.default
+          naisdevice.nixosModules.naisdevice
         ];
       };
     };
