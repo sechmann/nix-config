04e4157f55cec27bbc951e89ea47670b6a04aed0
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2024-07-17 10:26:14 +0200

    disable zed overlay

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,5 +2,5 @@
   hn-text = pkgs.callPackage ./hn-text {};
   fido2ble-to-uhid = pkgs.callPackage ./fido2ble-to-uhid {};
   # borrowed from https://github.com/SonarMonkey/meninx/tree/0eb42efa8582470aed516926646e8888cfbea365/pkgs/zed-editor
-  zed-editor = pkgs.callPackage ./zed-editor {};
+  #zed-editor = pkgs.callPackage ./zed-editor {};
 }
