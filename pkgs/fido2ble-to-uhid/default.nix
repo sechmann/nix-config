2f4980243002bcf63f6116a2de0377222fb8ad2e
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2024-07-17 12:11:44 +0200

    fmt

--- pkgs/fido2ble-to-uhid/default.nix
+++ pkgs/fido2ble-to-uhid/default.nix
@@ -6,11 +6,11 @@
 in
   python3Packages.buildPythonPackage {
     pname = "fido2ble-to-uhid";
+    version = rev;
     src = fetchFromGitHub {
       owner = "PoneBiometrics";
       repo = "fido2ble-to-uhid";
       rev = rev;
       sha256 = "sha256-+FeACk//JaDKPNv1GV4D4BvmSVhy9qDAqzPKY6aiJiw=";
     };
-    version = rev;
   }
