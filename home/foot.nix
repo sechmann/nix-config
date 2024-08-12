2332f8d7e694638752f5b913f118b94607c435bc
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2024-08-12 14:54:55 +0200

    add font awesome fallback

--- home/foot.nix
+++ home/foot.nix
@@ -4,7 +4,7 @@
     server.enable = true;
     settings = {
       main = {
-        font = "Source Code Pro:size=12";
+        font = "Source Code Pro:size=12,Font Awesome 6 Free:size=12,Font Awesome 6 Brands:size=12";
       };
 
       mouse = {
