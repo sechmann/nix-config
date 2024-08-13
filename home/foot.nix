d3027ef3c5ec5262305edf6049cddf5f346be64d
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2024-08-13 16:13:13 +0200

    foot color scheme

--- home/foot.nix
+++ home/foot.nix
@@ -1,10 +1,15 @@
-{...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: {
   programs.foot = {
     enable = true;
     server.enable = true;
     settings = {
       main = {
         font = "Source Code Pro:size=12,Font Awesome 6 Free:size=12,Font Awesome 6 Brands:size=12";
+        include = "${lib.getOutput "themes" pkgs.foot}/share/foot/themes/srcery";
       };
 
       mouse = {
