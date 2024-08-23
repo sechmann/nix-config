94936bb9a7ef8b948e4e1725063043e8d6e66752
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2024-08-23 09:48:54 +0200

    add add sql proxy

--- home/programs.nix
+++ home/programs.nix
@@ -14,6 +14,7 @@ in {
     ++ (with pkgs; [
       #zed-editor
       (google-cloud-sdk.withExtraComponents [google-cloud-sdk.components.gke-gcloud-auth-plugin])
+      google-cloud-sql-proxy
       azure-cli
       bitwarden-cli
       brightnessctl
