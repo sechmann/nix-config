66f3eaff28fc060629be9d44d27baea4c13acc91
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2024-08-26 07:06:10 +0200

    add zen-browser-bin pkg

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -4,4 +4,5 @@
   humanlog = pkgs.callPackage ./humanlog {};
   # borrowed from https://github.com/SonarMonkey/meninx/tree/0eb42efa8582470aed516926646e8888cfbea365/pkgs/zed-editor
   #zed-editor = pkgs.callPackage ./zed-editor {};
+  zen-browser-bin = pkgs.callPackage ./zen-browser-bin {};
 }
