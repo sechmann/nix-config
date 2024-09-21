b627daf25a4414e8a5d574ae902e1024cf365f0c
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2024-09-22 00:56:00 +0200

    intercept-browser: hard-code google user for certain urls

--- home/scripts/intercept-browser.sh
+++ home/scripts/intercept-browser.sh
@@ -25,16 +25,24 @@ handle_google_login() {
 }
 
 open_in_container() {
-	account="$(gcloud auth list --filter='active' --format 'value(account)')"
+	if [[ -z $2 ]]; then
+		account="$(gcloud auth list --filter='active' --format 'value(account)')"
+	else
+		account="$2"
+	fi
 
 	echo "ext+container:name=${account}&url=$(urlencode "$1")"
 }
 
 if [[ $url =~ https://accounts.google.com/o/oauth2/auth ]]; then
 	url=$(handle_google_login "$url")
-elif [[ $url =~ (fasit|monitoring).nais.io ]]; then
-	url=$(handle_google_login "$url")
-elif [[ $url =~ https://(www.)?youtube.com ]] || [[ $url =~ https://(www.)?youtu.be ]]; then
+elif [[ $url =~ https://(www\.)?.*nav\.cloud\.nais\.io ]]; then
+	url=$(open_in_container "$url" "vegar.sechmann.molvig@nav.no")
+elif [[ $url =~ https://(www\.)?.*\.nais\.io ]]; then
+	url=$(open_in_container "$url" "vegar@nais.io")
+elif [[ $url =~ https://(www\.)?.*twitch\.com ]]; then
+	url=$(open_in_container "$url" "personal")
+elif [[ $url =~ https://(www\.)?youtube.com ]] || [[ $url =~ https://(www.)?youtu.be ]]; then
 	mpv "$url"
 	exit 0
 elif [[ $url =~ https?://[^.]*.?medium.com ]]; then
